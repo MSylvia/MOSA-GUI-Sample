422ba6ce20bd8714ec555774f2efea476037c9ba
nifanfa <nifanfa@foxmail.com>
2021-08-25 17:28:11 +0800

    Update Dock.cs

--- MOSA1/Apps/Dock.cs
+++ MOSA1/Apps/Dock.cs
@@ -20,7 +20,7 @@ namespace MOSA1.Apps
         public override void UIUpdate()
         {
             System.Graphics.DrawFilledRectangle(0x313131, X, Y, Width, Height);
-            s = CMOS.Hour.ToString("X2") + ":" + CMOS.Minute.ToString("X2").PadLeft(2, '0');
+            s = CMOS.Hour.ToString() + ":" + CMOS.Minute.ToString().PadLeft(2, '0');
             System.Graphics.DrawBitFontString("ArialCustomCharset16", 0xFFFFFFFF, s, Width - BitFont.Calculate("ArialCustomCharset16", s) - Height, Y + (Height / 2 - 8));
         }
     }
